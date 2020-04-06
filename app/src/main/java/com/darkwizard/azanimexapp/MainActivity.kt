cbb1d778a5692ac681c49c039e64a299c39b6c4c
Julio Olivares <julioolivares90@hotmail.com>
2020-04-06 16:48:42 -0600

    Initial commit

---
+++ app/src/main/java/com/darkwizard/azanimexapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.darkwizard.azanimexapp
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    fun getTypeFace() : Typeface =Typeface.createFromAsset(assets,"fonts/orbitron_medium.ttf")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textLogo.typeface = getTypeFace()
+        textSlogan.typeface = getTypeFace()
+    }
+}
