963436a995155c8f11d44772a9ac3c40414ae832
Julio Olivares <julioolivares90@hotmail.com>
2020-04-08 18:36:10 -0600

    add botton navigationview

--- app/src/main/java/com/darkwizard/azanimexapp/MainActivity.kt
+++ app/src/main/java/com/darkwizard/azanimexapp/MainActivity.kt
@@ -14,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //textLogo.typeface = getTypeFace()
         //textSlogan.typeface = getTypeFace()
+
+
     }
 }
