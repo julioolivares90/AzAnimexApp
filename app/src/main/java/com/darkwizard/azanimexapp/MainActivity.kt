9e5369720165e93d863164f67c5941420ba710cd
Julio Olivares <julioolivares90@hotmail.com>
2020-04-17 15:15:25 -0600

    falta repara BottonMenu

--- app/src/main/java/com/darkwizard/azanimexapp/MainActivity.kt
+++ app/src/main/java/com/darkwizard/azanimexapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.darkwizard.azanimexapp
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //textLogo.typeface = getTypeFace()
         //textSlogan.typeface = getTypeFace()
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_inicio,R.id.navigation_pelicullas,
+            R.id.navigation_onas,R.id.navigation_ovas,
+            R.id.navigation_especiales
+        ))
+        val navController = NavController(this)
+        setupBottonNavMenu(navController)
+    }
 
+    private fun setupBottonNavMenu(navController : NavController){
+        val bottomNav = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNav?.setupWithNavController(navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
